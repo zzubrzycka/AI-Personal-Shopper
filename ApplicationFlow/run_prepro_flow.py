ca58e4932622020d2926232ecce6959a3234f1f1
rogerUrbieta <urbieta.roger@gmail.com>
2024-05-15 17:44:44 +0200

    application flow initial

---
+++ ApplicationFlow/run_prepro_flow.py
@@ -0,0 +1,17 @@
+import os
+from generics import RUN_PREPRO_PATH
+
+
+
+def run_prepro(user_img_path):
+    command = f"conda run -n prepro --cwd {RUN_PREPRO_PATH} python3 run.py --user_img {user_img_path}"
+    os.system(command)
+
+
+
+if __name__ == '__main__':
+    # Define the command
+    command = "conda run -n prepro --cwd /home/user/PreProcessing python3 run.py --user_img eugenia.jpg"
+
+    # Run the command using os.system()
+    os.system(command)
