0ea4c0f558069f6c78e848fa83efde2e337f1b4f
bebiner <ben.margott.sn@gmail.com>
2024-05-27 18:34:55 +0200

    [Graphical Design updated]

--- GUI/GUI2.py
+++ GUI/GUI2.py
@@ -2,51 +2,26 @@ import sys
 import os
 import shutil
 import subprocess
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QTabWidget, QLabel,
-                             QPushButton,
-                             QFileDialog, QDialog, QGridLayout, QMessageBox, QSpacerItem, QSizePolicy, QComboBox,
-                             QLineEdit, QFormLayout)
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel,
+                             QPushButton, QFileDialog, QDialog, QMessageBox, QGridLayout, QComboBox, QLineEdit, QFormLayout)
 from PyQt5.QtGui import QPixmap, QFont
 from PyQt5.QtCore import Qt
-#from AI_Personal_Shopper.ApplicationFlow.application_flow import return_final_pictures
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         # Set up main window properties
-        self.setWindowTitle("Welcome to AI Personal Shopper!")
+        self.setWindowTitle("AI Personal Shopper")
         self.setGeometry(100, 100, 1250, 600)
+        self.setStyleSheet("background-color: #f5f6f1;")  # Soft gray background
 
-        # Tab widget
-        self.tabs = QTabWidget()
-        self.setCentralWidget(self.tabs)
+        # Initialize main content widget and layout
+        self.main_widget = QWidget()
+        self.setCentralWidget(self.main_widget)
+        self.setup_main_layout()
 
-        # Create the tabs
-        self.home_tab = QWidget()
-        self.avatar_tab = QWidget()
-        self.uploaded_images_tab = QWidget()
-        self.output_images_tab = QWidget()
-
-        # Add tabs
-        self.tabs.addTab(self.home_tab, "Home")
-        self.tabs.addTab(self.avatar_tab, "Avatars")
-        self.tabs.addTab(self.uploaded_images_tab, "User Images")
-        self.tabs.addTab(self.output_images_tab, "Output Images")
-
-        # Set up content for each tab
-        self.setup_home_tab()
-        self.setup_avatar_tab()
-        self.setup_uploaded_images_tab()
-        self.setup_output_images_tab()
-
-        # A list to store uploaded images
-        self.user_uploaded_images = []
-        # Store paths to images to be processed
-        self.image_1_path = None
-        self.image_2_path = None
-
-        # Ensure input and output directories exist
+        # Ensure directories exist
         self.input_dir = "input_images"
         self.user_input_image_dir = "user_input_image"
         self.garment_input_image_dir = "garment_input_image"
@@ -54,139 +29,83 @@ class MainWindow(QMainWindow):
         os.makedirs(self.input_dir, exist_ok=True)
         os.makedirs(self.output_dir, exist_ok=True)
 
-    def setup_home_tab(self):
-        # Create a horizontal layout to separate left and right sections
-        horizontal_layout = QHBoxLayout()
+    def setup_main_layout(self):
+        # Create a layout to separate left and right sections
+        main_layout = QHBoxLayout(self.main_widget)
 
-        # Left side: title and instructions
+        # Left side: title and picture
         vertical_layout1 = QVBoxLayout()
+        
+        # Load and display the guide image
+        guide_image = QLabel()
+        guide_image_path = "guide.png"
+        pixmap = QPixmap(guide_image_path)
+        if pixmap.isNull():
+            print("Failed to load guide image.")
+        else:
+            guide_image.setPixmap(pixmap.scaled(675, 900, Qt.KeepAspectRatio, Qt.SmoothTransformation))
+        vertical_layout1.addWidget(guide_image)
 
-        title = QLabel("Welcome to AI Personal Shopper!")
-        title_font = QFont("Arial", 20, QFont.Bold)
-        title.setFont(title_font)
-        vertical_layout1.addWidget(title)
-
-        introduction_text = """
-                <p>Explore our virtual fitting room designed to make your fashion journey effortless and fun. Here's what you'll find:</p>
-                <ul>
-                    <li><b>Home:</b> Upload your own images to try on clothing items virtually. Select two images, blend them, and preview your new look!</li>
-                    <li><b>Avatars:</b> Access a collection of ready-to-use avatars for virtual fitting. Choose one and see how it enhances your style.</li>
-                    <li><b>User Images:</b> Browse previously uploaded images for easy access and reuse. Quickly apply them to new clothing combinations.</li>
-                    <li><b>Output Images:</b> View all your virtual fitting results in one place. Admire the blended images generated through our application.</li>
-                </ul>
-                <br>
-                <br>
-                <p>
-                How to use our app:
-                    <ol type="1">
-                    <li>Upload a picture of yourself by clicking the first button.</li>
-                    <li>Upload the picture of the clothing by clicking the second button.</li>
-                    <li>Click the "Try it on!" button.</li>
-                    <li>View the results.</li>
-                    </ol>
-                </p>
-                
-                <br>
-                <p>Get started and discover the future of fashion with <br>AI Personal Shopper!</p>
-                """
-
-        introduction = QLabel(introduction_text)
-        introduction_font = QFont("Arial", 12)
-        introduction.setMinimumWidth(350)
-        introduction.setFont(introduction_font)
-        introduction.setWordWrap(True)
-        vertical_layout1.addWidget(introduction)
-        vertical_layout1.addSpacing(50)
-
-        instructions = QLabel("Instructions:\n\n1. Upload a picture of yourself by clicking the first button.\n2. Upload the picture of the clothing by clicking the second button.\n3. Click the 'Try it on!' button\n4. View the results.")
-        instructions_font = QFont("Arial", 12)
-        instructions.setMaximumWidth(350)
-        instructions.setFont(instructions_font)
-        instructions.setWordWrap(True)
-        #vertical_layout1.addWidget(instructions)
-
-
-        # Right side: image loading
+        # Right side: image loading and button for image upload
         images_layout = QVBoxLayout()
-
-        # Button and image label for the first image
-        images_layout.addSpacing(20)
-        self.upload_button_1 = QPushButton("1. Upload a picture of yourself")
-        self.upload_button_1.setStyleSheet("font-size: 17px;")
-        self.upload_button_1.setMinimumWidth(300)
-        self.upload_button_1.setMinimumHeight(40)
-        self.image_label_1 = QLabel("No Image Loaded")
-        self.image_label_1.setAlignment(Qt.AlignCenter)
-        self.image_label_1.setFixedSize(250, 250)
-       # self.image_label_1.setAlignment(Qt.AlignCenter)
-        self.image_label_1.setStyleSheet("border: 2px solid black")
-        images_layout.addWidget(self.upload_button_1, alignment=Qt.AlignHCenter)
-        images_layout.addWidget(self.image_label_1, alignment=Qt.AlignHCenter)
-
-        # Add some extra vertical space
-        images_layout.addSpacing(40)
-
-        # Button and image label for the second image
-        self.upload_button_2 = QPushButton("2. Upload the picture of the clothing")
-        self.upload_button_2.setStyleSheet("font-size: 17px;")
-
-        self.upload_button_2.setMinimumWidth(300)
-        self.upload_button_2.setMinimumHeight(40)
-        self.image_label_2 = QLabel("No Image Loaded")
-        self.image_label_2.setAlignment(Qt.AlignCenter)
-        self.image_label_2.setFixedSize(250, 250)
-        self.image_label_2.setStyleSheet("border: 2px solid black")
-        images_layout.addWidget(self.upload_button_2, alignment=Qt.AlignHCenter)
-        images_layout.addWidget(self.image_label_2, alignment=Qt.AlignHCenter)
-        images_layout.addSpacing(40)
+        self.setup_image_upload_section(images_layout, 1, "Upload a picture of yourself")
+        self.setup_image_upload_section(images_layout, 2, "Upload the picture of the clothing")
 
         # "Run script" button to process the images
-        self.run_script_button = QPushButton("3. Try it on!")
-        self.run_script_button.setStyleSheet("background-color: green; color: white; font-size: 26px;")
+        self.run_script_button = QPushButton("Try it on!")
+        self.run_script_button.setStyleSheet(
+            "QPushButton {"
+            "background-color: #4CAF50;"
+            "color: white;"
+            "border-radius: 10px;"
+            "font-size: 18px;"
+            "padding: 10px;"
+            "}"
+            "QPushButton:hover {"
+            "background-color: #45a049;"
+            "}"
+        )
         self.run_script_button.setMinimumHeight(50)
-        self.run_script_button.setFixedWidth(250)
-        images_layout.addWidget(self.run_script_button, alignment=Qt.AlignHCenter)
-        images_layout.addSpacing(25)
-
-        # Connect button actions
-       # self.upload_button_1.clicked.connect(lambda: self.upload_image(1))
-       # self.upload_button_2.clicked.connect(lambda: self.upload_image(2))
-
-        self.upload_button_1.clicked.connect(lambda: self.open_popup(1))
-        self.upload_button_2.clicked.connect(lambda: self.open_popup(2))
-
         self.run_script_button.clicked.connect(self.process_images)
+        images_layout.addWidget(self.run_script_button, alignment=Qt.AlignHCenter)
 
-        vertical_layout3 = QVBoxLayout()
+        # Organize layouts
+        main_layout.addLayout(vertical_layout1)
+        main_layout.addLayout(images_layout)
 
-        self.output_label = QLabel("Your Output:")
-        self.output_label.setFont(QFont("Arial", 14, QFont.Bold))
+    def setup_image_upload_section(self, layout, index, text):
+        button = QPushButton(text)
+        button.setStyleSheet(
+            "QPushButton {"
+            "font-size: 16px;"
+            "background-color: #ec98b8;"  
+            "color: black;"
+            "border-radius: 5px;"
+            "padding: 12px;"
+            "margin: 5px;"
+            "}"
+            "QPushButton:hover {"
+            "background-color: #f7b3cc;"
+            "}"
+        )
+        button.setMinimumWidth(300)
+        button.setMinimumHeight(50)
+        label = QLabel("No Image Loaded")
+        label.setAlignment(Qt.AlignCenter)
+        label.setFixedSize(250, 250)
+        label.setStyleSheet("border: 1px solid #ccc;")
+        layout.addWidget(button, alignment=Qt.AlignHCenter)
+        layout.addWidget(label, alignment=Qt.AlignHCenter)
+
+        # Ensure the lambda captures the index and passes it to the function
+        button.clicked.connect(lambda: self.open_popup(index))
+
+        if index == 1:
+            self.image_label_1 = label
+        else:
+            self.image_label_2 = label
 
 
-        self.output_image_label = QLabel("No Output Image Yet")
-        self.output_image_label.setAlignment(Qt.AlignCenter)
-        #self.output_image_label.setMinimumHeight(300)
-        #self.output_image_label.setMinimumWidth(250)
-        self.output_image_label.setFixedSize(300, 400)
-        self.output_image_label.setStyleSheet("border: 3px solid black")
-
-        vertical_layout3.addSpacing(100)
-        vertical_layout3.addWidget(self.output_label, alignment=Qt.AlignHCenter)
-        vertical_layout3.addWidget(self.output_image_label, alignment=Qt.AlignHCenter)
-        vertical_layout3.addSpacing(150)
-
-        # Set stretch to control the relative space
-        vertical_layout3.setStretch(0, 1)  # Adjust the stretch factor of the label
-        vertical_layout3.setStretch(1, 3)  # Adjust the stretch factor of the image
-
-        horizontal_layout.addLayout(vertical_layout1)
-        horizontal_layout.addLayout(images_layout)
-
-        horizontal_layout.setStretch(1, 500)
-        horizontal_layout.addLayout(vertical_layout3)
-
-        #horizontal_layout.addLayout(vertical_layout3)
-        self.home_tab.setLayout(horizontal_layout)
 
     def open_popup(self, index):
         popup = PopupDialog(self, index)
@@ -219,48 +138,34 @@ class MainWindow(QMainWindow):
         self.output_images_layout = QVBoxLayout()
         self.output_images_tab.setLayout(self.output_images_layout)
 
+
+
     def upload_image(self, index):
-        # Open a file dialog to select an image
         image_path, _ = QFileDialog.getOpenFileName(self, "Select an Image", "", "Images (*.png *.jpg *.jpeg *.bmp)")
-        print(image_path)
         if image_path:
             image_name = os.path.basename(image_path)
 
-        if index == 1:
+            if index == 1:
+                target_dir = self.user_input_image_dir
+            else:
+                target_dir = self.garment_input_image_dir
+            
+            target_path = os.path.join(target_dir, image_name)
 
-            target_path2 = os.path.join(self.user_input_image_dir, image_name)
-            print(target_path2)
+            try:
+                if not os.path.exists(target_dir):
+                    os.makedirs(target_dir, exist_ok=True)
+                    print(f"Directory created: {target_dir}")
+                
+                shutil.copy(image_path, target_path)
+                print(f"File copied to {target_path}")
+                
+                self.set_image_label(index, target_path)
 
-            for item in os.listdir(self.user_input_image_dir):
-                item_path = os.path.join(self.user_input_image_dir, item)
-                os.remove(item_path)
+            except Exception as e:
+                QMessageBox.critical(self, "File Copy Error", f"An error occurred while copying file: {str(e)}")
+                print(f"Error copying file: {e}")
 
-
-            #shutil.rmtree(self.user_input_image_dir)
-            shutil.copy(image_path, target_path2)
-
-        if index ==2:
-            target_path2 = os.path.join(self.garment_input_image_dir, image_name)
-            print(target_path2)
-            #shutil.rmtree(self.garment_input_image_dir)
-
-            for item in os.listdir(self.garment_input_image_dir):
-                item_path = os.path.join(self.garment_input_image_dir, item)
-                os.remove(item_path)
-
-            shutil.copy(image_path, target_path2)
-
-
-        # Copy the image to the input_images directory
-
-        target_path = os.path.join(self.input_dir, image_name)
-        shutil.copy(image_path, target_path)
-        self.set_image_label(index, target_path)
-
-        # Add the uploaded image to the "User Images" tab
-        user_image_label = QLabel()
-        user_image_label.setPixmap(QPixmap(target_path).scaled(100, 100, Qt.KeepAspectRatio))
-        self.uploaded_images_layout.addWidget(user_image_label)
 
     def upload_avatar(self, index, avatar_path):
         print("jestesmy w funkcji upload avatar")
@@ -365,50 +270,64 @@ class MainWindow(QMainWindow):
         self.avatar_tab.setLayout(layout)
 
 class PopupDialog(QDialog):
-
     def __init__(self, main_window, index):
         super().__init__(main_window)
-
         self.main_window = main_window
         self.index = index
-
         self.setWindowTitle("Picture source")
+        self.setStyleSheet("background-color: #f5f5f5;")  # Consistent background color with the main window
 
+        # Setup labels and buttons
         self.label = QLabel("Choose the source of your picture:")
-
+        self.label.setFont(QFont("Arial", 12))  # Optional: Set font here if needed
         self.prev_uploaded_pic_button = QPushButton("Choose from previously uploaded pictures")
         self.avatars_button = QPushButton("Choose from avatars")
         self.new_pic_button = QPushButton("Upload a new picture")
 
-        # Tworzymy układ przycisków
+        # Apply consistent button styling
+        button_style = """
+        QPushButton {
+            font-size: 16px;
+            background-color: #007BFF;
+            color: white;
+            border-radius: 5px;
+            padding: 12px;
+            margin: 5px;
+        }
+        QPushButton:hover {
+            background-color: #0069D9;
+        }
+        """
+        self.prev_uploaded_pic_button.setStyleSheet(button_style)
+        self.avatars_button.setStyleSheet(button_style)
+        self.new_pic_button.setStyleSheet(button_style)
+
+        # Layout for buttons
         button_layout = QHBoxLayout()
         button_layout.addWidget(self.prev_uploaded_pic_button)
         button_layout.addWidget(self.avatars_button)
         button_layout.addWidget(self.new_pic_button)
 
-        # Tworzymy układ główny
+        # Main layout setup
         layout = QVBoxLayout()
         layout.addWidget(self.label)
         layout.addLayout(button_layout)
-
         self.setLayout(layout)
 
-
-        self.new_pic_button.clicked.connect(self.upload_new_picture)
-        self.avatars_button.clicked.connect(self.open_measurementsdialog)
-
+        # Connecting signals to slots
         self.prev_uploaded_pic_button.clicked.connect(self.close)
-        self.avatars_button.clicked.connect(self.close)
-        self.new_pic_button.clicked.connect(self.close)
+        self.avatars_button.clicked.connect(lambda: self.open_measurementsdialog(index))
+        self.new_pic_button.clicked.connect(lambda: self.upload_new_picture())
 
     def upload_new_picture(self):
         self.main_window.upload_image(self.index)
         self.accept()
 
     def open_measurementsdialog(self, index):
+        # Make sure to define how to handle opening measurement dialog if it's needed
         popup = MeasurementsDialog(self.main_window, index)
-
         popup.exec_()
+
 
 
 
@@ -426,7 +345,7 @@ class MeasurementsDialog(QDialog):
         self.label.setAlignment(Qt.AlignCenter)
 
         self.gender_label = QLabel("Gender:")
-        self.gender_combobox = QComboBox()
+        self.gender_combobox = QComboBox() 
         self.gender_combobox.addItems(["Woman", "Man"])
 
         # Fields for measurements
