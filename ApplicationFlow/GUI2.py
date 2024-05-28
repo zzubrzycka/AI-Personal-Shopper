ce1a83755f6795b2f1c1f80a765587ec495ea520
zzubrzycka <zoszub@gmail.com>
2024-05-28 13:14:30 +0200

    saving all outputs to one folder (additionally)

--- ApplicationFlow/GUI2.py
+++ ApplicationFlow/GUI2.py
@@ -379,6 +379,7 @@ class MainWindow(QMainWindow):
             pixmap = QPixmap(output_path_to_show).scaled(300, 400, Qt.KeepAspectRatio)
             self.output_image_label.setPixmap(pixmap)
             shutil.copy(output_path_to_show, self.output_dir)
+            shutil.copy(output_path_to_show, output_image_path)
             # Add the output image to the "Output Images" tab
             output_label = QLabel()
             output_label.setPixmap(QPixmap(output_path_from_script).scaled(200, 200, Qt.KeepAspectRatio))
